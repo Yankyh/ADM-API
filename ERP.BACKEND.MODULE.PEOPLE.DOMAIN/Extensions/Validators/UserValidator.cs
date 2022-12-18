f20b5b65badf9e577795af8ed52f27acb08fe5fc
Kohzido <matheus.zr@hotmail.com>
2022-12-18 18:04:09 +0000

    ExtensionValidator.

---
+++ ERP.BACKEND.MODULE.PEOPLE.DOMAIN/Extensions/Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace ERP.BACKEND.MODULE.PERSON.DOMAIN.Extensions.Validators
+{
+    using ERP.BACKEND.MODULE.PERSON.DOMAIN.Entities;
+    using System.ComponentModel.DataAnnotations;
+
+    public static class UserValidator
+    {
+        public static void Validate(this User user)
+        {
+            if (new EmailAddressAttribute().IsValid(user.Email))
+            {
+                throw new InvalidDataException($"O e-mail {user.Email} informado para o usuário {user.Name} é inválido.");
+            }
+        }
+    }
+}
